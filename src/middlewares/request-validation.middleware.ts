07fdb799d275331e82ccded615e798b59feb001c
Esmail Elmoussel <elmoussel12@gmail.com>
2023-07-28 14:46:07 +0300

    feat(RequestValidation): validate body and query

--- src/middlewares/request-validation.middleware.ts
+++ src/middlewares/request-validation.middleware.ts
@@ -2,12 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import Joi from "joi";
 import { RequestValidationError } from "../errors";
 
-export const requestValidationMiddleware = (schema: Joi.ObjectSchema) => {
+export const requestValidationMiddleware = (
+  schema: Joi.ObjectSchema,
+  source: "body" | "query" = "body",
+) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body, {
+    const { error, value } = schema.validate(req[source], {
       abortEarly: false,
       allowUnknown: false,
     });
+
+    req[source] = value;
 
     if (error) {
       throw new RequestValidationError(error.details);
