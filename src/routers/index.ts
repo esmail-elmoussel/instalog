35835aed9c9fa7a37968ecacd37280b0a6a52132
Esmail Elmoussel <elmoussel12@gmail.com>
2023-07-28 04:44:27 +0300

    feat: handle not found error

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 import { eventRouter } from "./event.router";
 import { loggerMiddleware } from "../middlewares";
+import {
+  NotFoundError,
+  errorHandlerMiddleware,
+} from "@esmailelmoussel/microservices-common";
 
 const router = express.Router();
 
 router.use(loggerMiddleware);
 
 router.get("/health", function (req, res) {
-  res.send(200);
+  res.sendStatus(200);
 });
 
 router.use("/events", eventRouter);
+
+router.all("*", () => {
+  throw new NotFoundError();
+});
+
+router.use(errorHandlerMiddleware);
 
 export { router };
