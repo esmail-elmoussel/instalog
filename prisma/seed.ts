dc2cd6880e7a92835122e3d39b9f5c38e803f0a5
Esmail Elmoussel <elmoussel12@gmail.com>
2023-07-28 16:06:41 +0300

    chore(seed): update users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,32 @@
 import { db } from "../src/container";
 import { logger } from "../src/utils";
 
+const mockUsers = [
+  { name: "Alice Johnson", email: "alice.johnson@example.com" },
+  { name: "Bob Smith", email: "bob.smith@example.com" },
+  { name: "Emma Watson", email: "emma.watson@example.com" },
+  { name: "John Doe", email: "john.doe@example.com" },
+  { name: "Lily Williams", email: "lily.williams@example.com" },
+  { name: "Michael Brown", email: "michael.brown@example.com" },
+  { name: "Sophia Lee", email: "sophia.lee@example.com" },
+  { name: "William Clark", email: "william.clark@example.com" },
+  { name: "Olivia Miller", email: "olivia.miller@example.com" },
+  { name: "James Taylor", email: "james.taylor@example.com" },
+];
+
 async function main() {
-  await db.user.upsert({
-    where: { email: "elmoussel12@gmail.com" },
-    update: {},
-    create: {
-      email: "elmoussel12@gmail.com",
-      name: "Esmail",
-      events: {
+  Promise.all(
+    mockUsers.map(async (user) => {
+      await db.user.upsert({
+        where: { email: user.email },
+        update: {},
         create: {
-          name: "test",
+          email: user.email,
+          name: user.name,
         },
-      },
-    },
-  });
+      });
+    }),
+  );
 }
 
 main()
